93168b179fad875ed324e0f83263a8135315b606
Giang Trường <hogiangtruong1@gmail.com>
2025-04-21 15:16:00 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import './assets/main.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free";
+import router from "./router";
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+createApp(App).use(router).mount("#app");
